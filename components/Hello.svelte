2786505f15466e7554f02d6d749208abff22ea64
kazuya kawaguchi <kawakazu80@gmail.com>
2022-09-16 19:29:18 +0900

    add svelte component

---
+++ components/Hello.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let text = 'hello';
+</script>
+      
+<div>
+  <div class="text-block-inner">
+    {text}
+  </div>
+</div>
+      
+<style>
+.text-block-inner {
+  width: 100%;
+}
+</style>
+      
